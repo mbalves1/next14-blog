2533b259b288d202fbc2e1d8de06e03e5f9ad08b
Murilo <mbalves1@outlook.com>
2024-10-01 16:20:43 -0300

    "Added 'use client' directive, new CardBorder component, and conditional rendering with state and event handling to Home component."

--- app/page.js
+++ app/page.js
@@ -1,20 +1,61 @@
+'use client'
 
-function Card({title}) {
+import { StrictMode, useState } from "react"
+
+function Card({title = "Nothing to say!"}) {
   return (
-    <div className="border rounded-md border-gray-600 p-4 w-[500px]">
+    <div className="border rounded-md border-gray-600 p-4 my-1">
       {title}
     </div>
   )
 }
 
+
+function CardBorder({children}) {
+  return (
+    <div className="border rounded-md border-gray-100 p-4 my-1">
+      {children}
+    </div>
+  )
+}
+
 export default function Home() {
+
+  const [isVisible, setIsVisible] = useState(true)
+  const handleClick = () => {    
+    return setIsVisible(!isVisible);
+  }
+  
   return (
     <>
       <div className="p-20">
         <div>
         Hello World!
         </div>
-        <Card title="This is being passed from the parent!" />
+
+        {
+          !isVisible ?
+          <div>
+            <Card title="This is being passed from the parent!" />
+            <Card title={"This is JS"} />
+            <Card />
+          </div> :
+          <div></div>
+        }
+                
+        {
+          isVisible ?
+          <CardBorder>
+            This is JS Visible
+          </CardBorder> :
+          <div></div>
+        }
+
+        <div>{ isVisible }</div>
+
+        <button className="border p-2 rounded-md m-2" onClick={handleClick}>
+          { isVisible ? 'Visible' : 'Not Visible' }
+        </button>
       </div>
     </>
   );
