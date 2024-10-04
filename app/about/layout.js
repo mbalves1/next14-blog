69823e2df576001f1ddb109064a10dff7b5e6416
Murilo <mbalves1@outlook.com>
2024-10-04 17:54:52 -0300

    [feat]: "Removed 'You might also like' section from AboutLayout and added API call to fetch repositories in ProjectsPage"

--- app/about/layout.js
+++ app/about/layout.js
@@ -4,15 +4,6 @@ export default function AboutLayout({ children }) {
     <>
       <div className="flex justify-center items-center h-screen">
         <div>{ children }</div>
-
-        <div>
-          <h2>You might also like</h2>
-
-          <ul>
-            <li>First blog post</li>
-            <li>Second blog post</li>
-          </ul>
-        </div>
       </div>
     </>
   )
--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,7 +1,24 @@
-export default function Projects() {
+export default async function ProjectsPage() {
+  const response = await fetch('http://localhost:3001/repos')
+  console.log('response', response);
+  
+  const repos = await response.json()
+  
   return (
     <>
-      Projects! This wont be visible!
+      <div className="p-20">
+        <h1 className="mb-8 text-xl">Projects</h1>
+
+        <ul>
+          { repos.map(repo => (
+            <li key={repo.id} className="mb-4">
+              <div>{repo.title}</div>
+              <div>{repo.description}</div>
+              <div>{repo.stargazers_count}</div>
+            </li>
+          )) }
+        </ul>
+      </div>
     </>
   )
 }
