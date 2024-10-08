bc562321668d50d9296cd55e0a6911f187d93b30
Murilo <mbalves1@outlook.com>
2024-10-08 14:46:53 -0300

    [feat]: "Added Link component from next/link and modified Header component structure"

--- components/header.js
+++ components/header.js
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import Navigation from "./navigation";
 
 export default function Header() {
   return (
-    <>
-      <header className="">
+    <header className="flex justify-between md:items-center mt-4">
+      <div className="flex items-center md:space-x-12">
+        <div className="hidden md:block">
+          <Link href="/" className="text-xl">Murilo Barros</Link>
+        </div>
         <Navigation></Navigation>
-      </header>
-    </>
+      </div>
+      <div>
+        Dark Toggle
+      </div>
+    </header>
   )
 }
