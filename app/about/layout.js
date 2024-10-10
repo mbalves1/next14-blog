c0f12d04593a241b0bbc212279a495579be45ea6
Murilo <mbalves1@outlook.com>
2024-10-10 08:58:08 -0300

    "Updated About page layout, removed error throws, and added project list component"

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,10 +1,17 @@
 export default function AboutLayout({ children }) {
 
   return (
-    <>
-      <div className="">
-        <div className="">{ children }</div>
+    <div>
+      <div>{ children }</div>
+
+      <div className="mt-8">
+        <h2 className="mb-4 text-xl">You might also like</h2>
+
+        <ul>
+          <li>First blog post</li>
+          <li>Second blog post</li>
+        </ul>
       </div>
-    </>
+    </div>
   )
 }
--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,3 @@
 export default function About() {
-  throw new Error('oooops')
   return <div>About Page Content</div>;
 }
