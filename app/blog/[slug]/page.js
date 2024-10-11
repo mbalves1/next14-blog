974509aa30cec6d132f10597118560fbd3c1f224
Murilo <mbalves1@outlook.com>
2024-10-11 09:13:43 -0300

    "Added import for 'notFound' and conditional check for 'params.slug' in BlogPage component"

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,6 +1,11 @@
-export default function BlogPage({ params }) {
+import { notFound } from "next/navigation"
 
+export default function BlogPage({ params }) {
   console.log(params)
+
+  if (!['first', 'second'].includes(params.slug)) {
+    notFound()
+  }
 
   return (
     <>
