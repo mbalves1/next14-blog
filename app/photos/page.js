9ba93b1a9887842171d7dad13c2dce8fbaae661c
Murilo <mbalves1@outlook.com>
2024-10-14 08:42:26 -0300

    [feat]: "Updated PhotosPage to use Next/Image component and imported images from public directory"

--- app/photos/page.js
+++ app/photos/page.js
@@ -1,20 +1,47 @@
+import Image from "next/image"
+
+import dog1 from '../../public/images/dog1.png'
+import dog2 from '../../public/images/dog2.png'
+import dog3 from '../../public/images/dog3.png'
+import dog4 from '../../public/images/dog4.png'
+
 export default function PhotosPage() {
   return (
     <div>
       <h1 className="text-2xl mb-8 font-semibold">My Photos</h1>
 
-      <div className="grid grid-cols-2 gap-4">
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog1.png" className="object-cover w-full h-full" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="relative h-60 overflow-hidden">
+          <Image
+            fill
+            src={dog1}
+            className="object-cover w-full h-full"
+            alt="Picture of my dog"
+            sizes="(max-width: 768px) 100vw, 50vw"/>
         </div>
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog2.png" className="object-cover w-full h-full" />
+        <div className="relative h-60 overflow-hidden">
+          <Image
+            fill
+            src={dog2}
+            className="object-cover w-full h-full"
+            alt="Picture of my dog"
+            sizes="(max-width: 768px) 100vw, 50vw"/>
         </div>
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog3.png" className="object-cover w-full h-full" />
+        <div className="relative h-60 overflow-hidden">
+          <Image
+            fill
+            src={dog3}
+            className="object-cover w-full h-full"
+            alt="Picture of my dog"
+            sizes="(max-width: 768px) 100vw, 50vw"/>
         </div>
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog4.png" className="object-cover w-full h-full" />
+        <div className="relative h-60 overflow-hidden">
+          <Image
+            fill
+            src={dog4}
+            className="object-cover w-full h-full"
+            alt="Picture of my dog"
+            sizes="(max-width: 768px) 100vw, 50vw"/>
         </div>
       </div>
     </div>
