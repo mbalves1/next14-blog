99d98986af73536a99452cf07fc1f8a3e8f21271
Murilo <mbalves1@outlook.com>
2024-10-16 08:57:37 -0300

    [feat]: install font and packages, adjustments to file names

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,5 +1,18 @@
 import { notFound } from "next/navigation"
 
+//const titles = {
+//  'first': 'hello first',
+//  'second': 'hello second',
+//}
+
+//export async function generateMetadata({ params, searchParams }, parent) {
+//  const description = (await parent).description ?? "Default description"
+//  return {
+//    title: titles[params.slug],
+//    description
+//  }//
+//}
+
 export default function BlogPage({ params }) {
   console.log(params)
 
