c558a7709d3df8c516bb0ac79c4d3802639386e1
Murilo <mbalves1@outlook.com>
2024-10-21 18:37:50 -0300

    [feat]: "Extracted H1 component and replaced hardcoded h1 elements with it in various pages and MDX configuration"

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -4,12 +4,13 @@ import { Suspense } from "react";
 import ProjectsList from "./components/project-list";
 import ProjectListLoading from "./components/project-list-loading";
 import { ErrorBoundary } from "react-error-boundary";
+import H1 from "@/components/h1";
 
 export default async function ProjectsPage() {
 
   return (
     <div>
-      <h1 className="mb-8 text-xl">Projects</h1>
+      <H1>Projects</H1>
 
       <ErrorBoundary fallback={<div>Cannot fetch projects currently</div>}>
         <Suspense fallback={<ProjectListLoading />}>
