a8c3741ff6656ba6dcb7093cce80835466fcd3b7
Murilo <mbalves1@outlook.com>
2024-10-21 11:36:24 -0300

    [feat]: "Updated BlogPostsPage to handle tags and order search params, and added link to toggle post order"

--- app/blog/page.js
+++ app/blog/page.js
@@ -5,10 +5,12 @@ import Link from 'next/link';
 export default async function BlogPostsPage({ searchParams }) {
 
   console.log(searchParams.tags);
-  const tags = searchParams.tags.split(',')
-  console.log('tags', tags);
-  
-  const posts = await getPosts();
+  const tags = searchParams.tags?.split(',')
+  const order = searchParams.order ?? 'newest'
+  const posts = await getPosts({
+    tags,
+    newest: order === 'newest'
+  });
 
   return (
     <>
@@ -16,6 +18,12 @@ export default async function BlogPostsPage({ searchParams }) {
       <div className="text-lg text-gray-600 dark:text-gray-400 mb-8">Stay up to date with most recent posts</div>
 
       <hr/>
+
+      <div className='mb-8'>
+        Display&nbsp;
+        { order === 'newest' && <Link href="/blog?order=oldest" className="underline">oldest</Link> }
+        { order === 'oldest' && <Link href="/blog?order=newest" className="underline">newest</Link> }
+      </div>
 
       <ul className="grid grid-cols-1 md:grid-cols-2 gap-8">
         { posts.map(post => (
