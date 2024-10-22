233cac8000d524186b858a11bd41b09a6a9e7280
Murilo <mbalves1@outlook.com>
2024-10-22 09:50:11 -0300

    [feat]: "Added dark mode toggle, post tags with links, and updated tailwind config"

---
+++ components/dark-mode.js
@@ -0,0 +1,18 @@
+"use client"
+
+import useDarkMode from "@/hooks/use-dark-mode";
+
+const nextModeIcons = {
+  "light": "🌑",
+  "dark": "🌞",
+}
+
+export default function DarkMode() {
+const { theme, toggleTheme } = useDarkMode()
+  
+  return (<>
+      <button onClick={toggleTheme}>
+        { nextModeIcons[theme] }
+      </button>
+    </>)
+}
---
+++ hooks/use-dark-mode.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const useDarkMode = (defaultTheme = "dark") => {
+  const [ theme, setTheme ] = useState(defaultTheme);
+  const setAndSaveTheme = (theme) => {
+    setTheme(theme);
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+  };
+
+  const toggleTheme = () => {
+    setAndSaveTheme(theme === "dark" ? "light" : "dark")
+  }
+
+  return {
+    toggleTheme,
+    theme
+  }
+}
+
+export default useDarkMode;
